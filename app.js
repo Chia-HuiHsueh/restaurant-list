276d1f6454d0e41d43c971f2736abc5400065b54
Ashley <jlmax1222@gmail.com>
2021-01-15 22:03:14 +0800

    Load JSON into Express

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -11,7 +12,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
